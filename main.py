4dc97312412cc9f95b5d6e680aa48f9fa5961cdc
Andrey <moroz12032005@gmail.com>
2025-09-24 16:05:17 +0300

    All RV's has been added

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ Nsim = 100000
 X = norm.rvs(size=(Nsim, n))
 
 X_sorted = np.sort(X, axis=1)
-X2 = X_sorted[:, 1]
 
-U = norm.cdf(X2)
+U1 = n * norm.cdf(X_sorted[:, 1])
+U2 = n * (1 - norm.cdf(X_sorted[:, -3]))
 
-Y = n * U
+mean = np.mean(X_sorted, axis=1)
+var = np.var(X_sorted, axis=1)
+median = np.quantile(X_sorted, 0.5, axis=1)
 
-plt.hist(Y, bins=100, density=True, alpha=0.6, color='skyblue', label='Эмпирика (Normal)')
-x_vals = np.linspace(0, np.max(Y), 500)
+plt.hist(U1, bins=100, density=True, alpha=0.6, color='skyblue', label='Эмпирика (Normal)')
+x_vals = np.linspace(0, np.max(U1), 500)
 plt.plot(x_vals, gamma.pdf(x_vals, a=2, scale=1), 'r-', lw=2, label='Gamma(2,1)')
 
 plt.xlabel("y")
